46bdad614c54c31cad004faf9e94b05111fff47e
Anamika-s <anamika.sawhney22@gmail.com>
2023-11-03 14:13:39 +0530

    Add project files.

---
+++ WebApi/NewFolder/EmployeeRepository.cs
@@ -0,0 +1,62 @@
+﻿using WebAppDemo.Context;
+using WebAppDemo.Models;
+using WebAppWithLayers.IRepository;
+
+namespace WebAppWithLayers
+{
+    public class EmployeeRepository : IEmplyeeRepo
+    {
+        EmployeeDbContext _context;
+        public EmployeeRepository(EmployeeDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateEmployee(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+
+        }
+
+        public int DeleteEmployee(int id)
+        {
+            var emp = _context.Employees.FirstOrDefault(x => x.Id == id);
+            if (emp != null)
+            {
+                _context.Employees.Remove(emp);
+                _context.SaveChanges();
+                return 1;
+            }
+            else
+                return 0;
+        }
+    
+
+        public int EditEmployee(int id, Employee employee)
+        {
+            var emp = _context.Employees.FirstOrDefault(x => x.Id == id);
+            if (emp != null)
+            {
+                emp.Dept = employee.Dept;
+                emp.Salary = employee.Salary;
+                
+                _context.SaveChanges();
+                return 1;
+            }
+            else
+                return 0;
+
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+           return _context.Employees.ToList();
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            return _context.Employees.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
---
+++ WebApi/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using WebAppDemo.Context;
+using WebAppWithLayers.IRepository;
+using WebAppWithLayers;
+
+namespace WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDbContext<EmployeeDbContext>
+                (op => op.UseSqlServer(builder.Configuration["ConnectionStrings:MyConnection"]));
+
+            builder.Services.AddScoped<IEmplyeeRepo, EmployeeRepository>();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
